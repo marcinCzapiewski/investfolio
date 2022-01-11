a17eb5eca816e01edc0a5adda52f413e69b064fe
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2022-01-11 23:27:00 +0100

    Add swagger

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureServices(builder.Services);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+Configure(app);
+
 app.Run();
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+}
+
+void Configure(WebApplication webApp)
+{
+    webApp.UseSwagger();
+    webApp.UseSwaggerUI();
+}
